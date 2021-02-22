3193521fa969c10fb1d308a5c22dd0fbd970b814
jojotheelephant <33424322+jojotheelephant@users.noreply.github.com>
2021-02-22 09:16:21 -0800

    Updates

---
+++ precache-manifest.fa981c59e9f16c8e8f1af0b0827dccbd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f557ef4c2dce9639bdd329b0cfcba1f",
+    "url": "/pomodoro/index.html"
+  },
+  {
+    "revision": "cdb83a9469e9bafe43aa",
+    "url": "/pomodoro/static/css/main.c76b5452.chunk.css"
+  },
+  {
+    "revision": "3fe03b2f3fb502ab0b56",
+    "url": "/pomodoro/static/js/2.da5843e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pomodoro/static/js/2.da5843e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdb83a9469e9bafe43aa",
+    "url": "/pomodoro/static/js/main.e69244d7.chunk.js"
+  },
+  {
+    "revision": "506bd58e222d5bcc2846",
+    "url": "/pomodoro/static/js/runtime-main.67091922.js"
+  }
+]);
